5f938244738fbe53e4f1ad7bd7de6ec4e611a099
ITDevNurperi <tenizbekovnanuru@gmail.com>
2023-07-31 12:14:23 +0600

    two way data binding

--- src/components/CreateInput.jsx
+++ src/components/CreateInput.jsx
@@ -6,22 +6,22 @@ import "./CreateInput.css"
 const CreateInput = () => {
 
     // 1-Вариант
-    // const [surname, setSurname] = useState("")
-    // const [email, setEmail] = useState("")
-    // const [password, setPassword] = useState("")
+    const [surname, setSurname] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
 
-    // const surnameHandler = (e) => {
-    //     setSurname(e.target.value)
-    // }
+    const surnameHandler = (e) => {
+        setSurname(e.target.value)
+    }
 
-    // const emilHandler = (e) => {
-    //     setEmail(e.target.value)
-    // }
+    const emilHandler = (e) => {
+        setEmail(e.target.value)
+    }
 
-    // const passwordHendler = (e) => {
-    //     setPassword(e.target.value)
-    // }
+    const passwordHendler = (e) => {
+        setPassword(e.target.value)
+    }
 
 
 
@@ -42,57 +42,57 @@ const CreateInput = () => {
 
 
     // 3-Вариант
-     const [userInput, setUserInput] = useState({
-        surname: "",
-        email: "",
-        password: ""
-    })
+    //  const [userInput, setUserInput] = useState({
+    //     surname: "",
+    //     email: "",
+    //     password: ""
+    // })
 
-    const surnameHandler = (e) => {
-        setUserInput((prevState) => {
-           return {
-            ...prevState,
-            surname: e.target.value
-           }
-        })
-    }
+    // const surnameHandler = (e) => {
+    //     setUserInput((prevState) => {
+    //        return {
+    //         ...prevState,
+    //         surname: e.target.value
+    //        }
+    //     })
+    // }
 
-    const emilHandler = (e) => {
-        setUserInput((prevState) => {
-            return {
-             ...prevState,
-             email: e.target.value
-            }
-         })
-    }
+    // const emilHandler = (e) => {
+    //     setUserInput((prevState) => {
+    //         return {
+    //          ...prevState,
+    //          email: e.target.value
+    //         }
+    //      })
+    // }
 
-    const passwordHendler = (e) => {
-        setUserInput((prevState) => {
-            return {
-             ...prevState,
-             password: e.target.value
-            }
-         })
-    }
+    // const passwordHendler = (e) => {
+    //     setUserInput((prevState) => {
+    //         return {
+    //          ...prevState,
+    //          password: e.target.value
+    //         }
+    //      })
+    // }
 
 
     const addInput = (e) => {
         e.preventDefault()
 
-        console.log(userInput);
-        // console.log(surname);
-        // console.log(email);
-        // console.log(password);
+        // console.log(userInput);
+        console.log(surname);
+        console.log(email);
+        console.log(password);
     }
 
     return (
         <Card className="input">
             <form onSubmit={addInput}>
-                <input onChange={surnameHandler} name='surname' type="text" placeholder='ФИО' />
+                <input value={surname} onChange={surnameHandler} name='surname' type="text" placeholder='ФИО' />
                 
-                <input onChange={emilHandler} name='email' type="text" placeholder='email' />
+                <input value={email} onChange={emilHandler} name='email' type="text" placeholder='email' />
 
-                <input onChange={passwordHendler} name='password' type="text" placeholder='password' />
+                <input value={password} onChange={passwordHendler} name='password' type="text" placeholder='password' />
 
                 <Button type='submit'>Add</Button>
             </form>
